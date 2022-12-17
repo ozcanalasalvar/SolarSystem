b03a47ad292d2b5ccafcd100611534a96d2eb8cb
OzcanAlasalvar <ozcanalasalvarr@gmail.com>
2022-12-18 01:40:59 +0300

    mock data is added.

---
+++ app/src/main/java/com/ozcan/alasalvar/solarsystemapp/data/Planet.kt
@@ -0,0 +1,11 @@
+package com.ozcan.alasalvar.solarsystemapp.data
+
+data class Planet(
+    val position: String,
+    val name: String,
+    val image: String,
+    val velocity: String,
+    val distance: String,
+    val description: String,
+    val color: Long
+)
