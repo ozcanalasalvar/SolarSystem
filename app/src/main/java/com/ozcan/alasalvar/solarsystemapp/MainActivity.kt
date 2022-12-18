f2c28e881f3e9cddfaa33f80e8c593ac559cbc93
OzcanAlasalvar <ozcanalasalvarr@gmail.com>
2022-12-18 05:35:02 +0300

    di impl

--- app/src/main/java/com/ozcan/alasalvar/solarsystemapp/MainActivity.kt
+++ app/src/main/java/com/ozcan/alasalvar/solarsystemapp/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.ui.Modifier
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.ozcan.alasalvar.solarsystemapp.presentation.ui.theme.SolarSystemAppTheme
 import com.ozcan.alasalvar.solarsystemapp.presentation.Navigation
+import dagger.hilt.android.AndroidEntryPoint
 
 @OptIn(ExperimentalPagerApi::class)
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/ozcan/alasalvar/solarsystemapp/PlanetsApplication.kt
@@ -0,0 +1,7 @@
+package com.ozcan.alasalvar.solarsystemapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PlanetsApplication : Application()
